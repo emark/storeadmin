7598cad75ff3a689d8d3e7132a45bf5a359c08a0
Maxim.Sviridenko <sviridenko.maxim@gmail.com>
2013-03-18 13:13:42 +0800

    add xmpp notify

--- notify.pl
+++ notify.pl
@@ -32,6 +32,7 @@ while (my $row = $result->fetch_hash){
 	$orderid = $row->{id};
 	my $tel = $row->{tel};
 	&SendSMS($tel) if length($tel)>8;
+	&XMPPNotify;
 	&ChangeStatus;
 };
 };
@@ -48,7 +49,11 @@ sub SendSMS(){
 my $ua = Mojo::UserAgent->new();
 my $tel = $_[0];
 my $sender_name = 'NaStart';
-my $decode_msg = "%D0%A1%D0%BF%D0%B0%D1%81%D0%B8%D0%B1%D0%BE%20%D0%B7%D0%B0%20%D0%B7%D0%B0%D0%BA%D0%B0%D0%B7%20%D0%BD%D0%BE%D0%BC%D0%B5%D1%80%20$orderid.%20%D0%9C%D0%B0%D0%B3%D0%B0%D0%B7%D0%B8%D0%BD%20%D0%9D%D0%B0%D0%A1%D1%82%D0%B0%D1%80%D1%82.%D0%A0%D0%A4%20(391)%2029-202-29";
+my $decode_msg = "%D0%92%D0%B0%D1%88%20%D0%B7%D0%B0%D0%BA%D0%B0%D0%B7%20($orderid)%20%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B5%D0%BD.%20%D0%92%D0%BE%D0%BF%D1%80%D0%BE%D1%81%D1%8B%20%D0%BF%D0%BE%20%D1%82%D0%B5%D0%BB.%20%2029-202-29%20%D0%9D%D0%B0%D0%A1%D1%82%D0%B0%D1%80%D1%82.%D0%A0%D0%A4";
 $ua->get("http://api.sms24x7.ru/?method=push_msg&email=sviridenko.maxim\@gmail.com&password=X53aRU1&sender_name=$sender_name&text=$decode_msg&phone=$tel&api_v=1.0&nologin=true&format=json");
-print "$orderid $tel";
+};
+
+sub XMPPNotify(){
+my @args = "echo 'Noviy zakaz ($orderid)' | sendxmpp emrk\@jabber.org dsfqtn\@cloudim.ru";
+system (@args) == 0 or die "Can't start programm";
 };
--- xmpp.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/perl -w
-
-use strict;
-use warnings;
-
-my @args = ("echo 'Welcome to JABBER' | sendxmpp emrk\@jabber.org");
-exec @args;
