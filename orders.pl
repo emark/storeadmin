8c9f61a0655da8e9e818cbe46d506d0f551ae377
Maxim.Sviridenko <sviridenko.maxim@gmail.com>
2013-06-04 13:29:17 +0800

    edit discount pritn templ

--- orders.pl
+++ orders.pl
@@ -149,49 +149,52 @@ CSS
 	print '</td><td align=right>';
 	print "<b>+7 (391) 203-03-10 | 292-02-29</b><br />Пн-Пт с 11:00 - 19:00<br />Сб. с 12:00 - 18:00";
 	print '</td></tr></table>';
-	print '<h2 align=center>Акт приема-передачи товара</h2>';
+	print '<h2 align=center>Акт передачи товара</h2>';
 	print "<p><b>Номер заказа:</b> $result->{id}-$cartid<br />";
 	print '<b>Продавец:</b> ООО "Электронный маркетинг", ИНН 2463213306, Красноярск,
 ул. Телевизорная,1с9<br />';
 	print "<b>Покупатель:</b> частное лицо $result->{person}, $result->{tel}, $result->{address}";
 	print '</p>';
+	my $discount = $result->{discount};
+
 	$result = $dbi->select(
         table => 'items',
-        column => [
-            'productid',
-            'title',
-            'count',
-            'price',
-			'id',
-        ],
 		where => {cartid => $cartid},
     );
     print '<table border=1 cellpadding=5 cellspacing=0>';
 	print '<tr><th>№</th><th>Наименование</th><th>Артикул</th><th>Кол-во</th><th>Цена</th><th>Сумма</th></tr>';
 	my $n = 1;
 	my $total = 0;
-	my $total_cost = 0;
+	my $discount_base = 0;
     while(my $row = $result->fetch_hash){
-		my $cost = $dbi->select(
-			table => 'products',
-			column => 'cost',
-			where => {id => $row->{productid}},
-		)->value;
         print '<tr>';
 		print "<td align=center>$n</td>";
 		print "<td>$row->{title}</td>";
 		print sprintf ("<td align=center>%06d</td>",$row->{productid});
 		print "<td align=right>$row->{count}</td>";
-		print "<td align=right>$row->{price}-00<span align=right class=noprint> / $cost-00</span></td>";
-		print sprintf "<td align=right>%d-00<span align=right class=noprint> / %d-00</span></td>",$row->{count}*$row->{price},$row->{count}*$cost;
+		print "<td align=right>$row->{price}-00</td>";
+		print sprintf "<td align=right>%d-00</td>",$row->{count}*$row->{price};
         print '</tr>';
 		$n++;
 		$total = $total + $row->{count}*$row->{price};
-		$total_cost =$total_cost + $cost * $row->{count};
+		$discount_base = $discount_base + $row->{price}*$row->{count} if !$row->{discount};
     };
     print '</table>';
-	print "<h3>Итого: $total руб. 00 коп.</h3>";
-	print p({class => "noprint"},"Себестоимость: $total_cost");
+
+	if($discount){
+		my $discount_rate = $dbi->select(
+			column => 'discount',
+			table => 'discounts',
+			where => {name => $discount},
+		)->value;
+		my $discount_sum = sprintf("%d",$discount_base*$discount_rate/100);
+		$total = $total-$discount_sum;
+		$total = "$total руб. в том числе скидка $discount_rate%",
+	}else{
+		$total = $total.' руб. 00 коп.';
+	};
+	
+	print "<h3>Итого: $total </h3>";
 	print p('<b>Гарантия на товары составляет 6 месяцев со дня продажи, если не указан иной срок.</b>');
 	print p('Товар получен и проверен. Претензий к ассортименту, количеству, внешнему виду, комплектации товара не имею.');
 	print '<table border=0>';
