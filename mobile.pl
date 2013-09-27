11c99b53ceb3f838a59e7589ab330c945740c405
Maxim.Sviridenko <sviridenko.maxim@gmail.com>
2013-09-27 12:45:37 +0800

    new mobile script

---
+++ mobile.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl -w
+
+use strict;
+use lib "/home/hosting_locumtest/usr/local/lib/perl5";
+use warnings;
+use utf8;
+use Mojo::UserAgent;
+use DBIx::Custom;
+use CGI;
+use v5.10;
+require "pkg/Common.pm";
+
+my %appcfg = Common::GetConfig();
+my $dbi = DBIx::Custom->connect(
+            dsn => $appcfg{'dsn'},
+            user => $appcfg{'user'},
+            password => $appcfg{'password'},
+            option => {mysql_enable_utf8 => 1}
+);
+
+$dbi->do('SET NAMES utf8');
+
+my $query = new CGI;
+my $cartid = $query->param('cid') || 0;
+
+print "Content-type: text/html\n\n";
+
+my $order = $dbi->select(
+	table => 'orders',
+	column => ['id','tel','address'],
+	where => {cartid => $cartid}
+)->fetch_all || undef;
+
+print "<p>Заказ: $order->[0][0]</p><p>$order->[0][1]</p><p><form action=\"http://maps.yandex.ru/?text=$order->[0][2]\" method=\"get\"><input type=\"submit\" value=\"Адрес на карте\"></form></p>";
