729f9fa166b2f3d717d56ff66bbd26606edba079
Maxim Sviridenko <sviridenko.maxim@gmail.com>
2014-11-28 10:20:24 +0700

    Edit phone number

--- sms.pl
+++ sms.pl
@@ -62,7 +62,7 @@ my $order = $dbi->select(
 my $ua = Mojo::UserAgent->new();
 my $tel = $order->{tel};
 my $orderid = $order->{id};
-my $decode_msg = "%D0%9D%D0%B0%D0%A1%D1%82%D0%B0%D1%80%D1%82.%D0%A0%D0%A4:%20%D0%B2%D0%B0%D1%88%20%D0%B7%D0%B0%D0%BA%D0%B0%D0%B7%20$orderid%20%D0%BF%D1%80%D0%B8%D0%BD%D1%8F%D1%82.%20%D0%9F%D0%BE%D0%B4%D1%80%D0%BE%D0%B1%D0%BD%D0%B5%D0%B5:%2083912030310";
+my $decode_msg = "%D0%9D%D0%B0%D0%A1%D1%82%D0%B0%D1%80%D1%82.%D0%A0%D0%A4:%20%D0%B2%D0%B0%D1%88%20%D0%B7%D0%B0%D0%BA%D0%B0%D0%B7%20$orderid%20%D0%BF%D1%80%D0%B8%D0%BD%D1%8F%D1%82.%20%D0%9F%D0%BE%D0%B4%D1%80%D0%BE%D0%B1%D0%BD%D0%B5%D0%B5:%2083912920229";
 
 print $ua->get("http://api.sms24x7.ru/?method=push_msg&email=$appcfg{sms24mail}&password=$appcfg{sms24pass}&text=$decode_msg&phone=$tel&api_v=1.1&nologin=true&format=json")->res->body;
 
